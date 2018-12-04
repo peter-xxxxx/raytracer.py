6fcf1b5a7d0d8a23a75bdb424d7fd1907079180b
Lincolnxyl <xuyuliang0110@163.com>
2018-12-04 16:00:33 -0500

    Sphere position and color marked

--- main.py
+++ main.py
@@ -15,35 +15,46 @@ width = 640
 height = 480
 super_sampling = 1
 
-# create demo scene
+"""
+create demo scene
+"""
+
+# RGB = (255, 255, 255) - White
 light_mat = Material(emission_color=Vector3(4, 4, 4))
 light_sphere = Sphere(Vector3(5, 50, 20), 5)
 light_obj = RenderObject(light_sphere, light_mat)
 
+# RGB = (255, 255, 255) - White
 light2_mat = Material(emission_color=Vector3(2, 2, 2))
 light2_sphere = Sphere(Vector3(0, 10, -5), 5)
 light2_obj = RenderObject(light2_sphere, light2_mat)
 
+# RGB = (0, 0, 255) - Blue
 blue_light_mat = Material(emission_color=Vector3(0, 0, 1))
 blue_light_sphere = Sphere(Vector3(-40, 5, 10), 3)
 blue_light_obj = RenderObject(blue_light_sphere, blue_light_mat)
 
+# GGB = (51, 51, 51) - Deep Grey
 ground_mat = Material(surface_color=Vector3(0.2, 0.2, 0.2))
 ground_sphere = Sphere(Vector3(0, -10010, 20), 10000)
 ground_obj = RenderObject(ground_sphere, ground_mat)
 
+# RGB = (204, 51, 51) - Deep Red
 mat1 = Material(surface_color=Vector3(0.8, 0.2, 0.2))
 sph1 = Sphere(Vector3(10, 1, 50), 5)
 obj1 = RenderObject(sph1, mat1)
 
+# RGB = (255, 255, 255) - Black
 mat2 = Material(surface_color=Vector3(1, 1, 1), transparency=1, ior=1.1)
 sph2 = Sphere(Vector3(1, -1, 10), 1)
 obj2 = RenderObject(sph2, mat2)
 
+# RGB = (204, 204, 255) - Purple
 mat3 = Material(surface_color=Vector3(0.8, 0.8, 1), reflectivity=0.5, transparency=0.8)
 sph3 = Sphere(Vector3(-4, -1, 20), 2)
 obj3 = RenderObject(sph3, mat3)
 
+# RGB = (255, 255, 255) - Black
 mat4 = Material(surface_color=Vector3(1, 1, 1), reflectivity=1.0)
 sph4 = Sphere(Vector3(0, 0, 60), 8)
 obj4 = RenderObject(sph4, mat4)
